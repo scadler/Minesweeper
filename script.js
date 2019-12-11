50e785f67ac7275955bf345e6aede10a39d93fc2
scadler <scadler@s.sfusd.edu>
2019-12-11 22:49:10 +0000

    tile images now work and are fully implemented

--- script.js
+++ script.js
@@ -3,6 +3,36 @@ $("#gameStart").click(function(){
     boardClear();
     plantMines();
 });
+function tileImagePicker(mineTileCounter, i){
+    if(mineTileCounter===0){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/8/80/Minesweeper_0.svg"></img>`);
+    }
+    else if(mineTileCounter===1){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Minesweeper_1.svg"></img>`);
+    }
+    else if(mineTileCounter===2){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/4/44/Minesweeper_2.svg"></img>`);
+    }
+    else if(mineTileCounter===3){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/0/08/Minesweeper_3.svg"></img>`);
+    }
+    else if(mineTileCounter===4){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Minesweeper_4.svg"></img>`);
+    }
+    else if(mineTileCounter===5){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/4/46/Minesweeper_5.svg"></img>`);
+    }
+    else if(mineTileCounter===6){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Minesweeper_6.svg"></img>`);
+    }
+    else if(mineTileCounter===7){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/5/56/Minesweeper_7.svg"></img>`);
+    }
+    else if(mineTileCounter===8){
+    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/0/0d/Minesweeper_8.svg"></img>`);
+    }
+    $("#"+i).css({"height": "50", "width": "50", "border":"0px"})
+}
 function plantMines(){
     var i = 0;
     while(i<12){
@@ -23,6 +53,7 @@ function boardClear(){
     while(i<79){
         $("#"+i).empty();
         i=i+1;
+        $("#"+i).css({"height": "40", "width": "40", "border-top":"5px solid #FBFAF9" , "border-left":"5px solid #FBFAF9", "border-bottom":"5px solid #949494", "border-right":"5px solid #949494"})
     }
 }
 function boardPopulate(){    
@@ -83,8 +114,8 @@ function boardPopulate(){
                 mineTileCounter=mineTileCounter+1;
             }
             console.log(mineTileCounter+" mineTileCounte id= "+i);
-            $("#" + i).append(mineTileCounter);
-
+            //$("#" + i).append(mineTileCounter);
+            tileImagePicker(mineTileCounter, i);
         }
         i=i+1;
         mineTileCounter=0;
