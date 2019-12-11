daeb365c454c57d537089fcf2f11ca8464a0d41f
scadler <scadler@s.sfusd.edu>
2019-12-11 02:46:00 +0000

    added bomb plant function

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $("#gameStart").click(function(){
-    console.log("works");
+    plantMines();
 });
+function plantMines(){
+    var i = 0;
+    while(i<12){
+    var xCoor = Math.floor(Math.random()*70);
+    var yCoor = Math.floor(Math.random()*8)
+    var xyCoor = Number(xCoor+yCoor);
+    //if($("#" + xyCoor).contents() === ""){
+    console.log(xyCoor);
+    //}
+    i = i+1;
+    }
+}
