3a92f45feac4f1c13e75662689fd61c3d5004d3d
scadler <scadler@s.sfusd.edu>
2019-12-13 21:00:56 +0000

    game dome, will work on adding more functionality

--- script.js
+++ script.js
@@ -1,37 +1,50 @@
-//https://commons.wikimedia.org/wiki/File:Minesweeper_1.svg
 $("#gameStart").click(function(){
     boardClear();
     plantMines();
 });
+$(".square").click(function(){
+  //var thisId = $(this).contents().attr('id');
+  var parentDivId = $(this).attr('id');
+    if($(this).contents().attr('class') !== "bomb"){ 
+        $("#"+parentDivId).css({"height": "50", "width": "50", "border":"0px"});
+        $("#"+$(this).contents().attr('id')).show();
+    }
+    if($(this).contents().attr('class') === "bomb"){
+        $(".bomb").show();
+        $("#"+$(this)).css({"height": "40", "width": "40", "border-top":"5px solid #FBFAF9" , "border-left":"5px solid #FBFAF9", "border-bottom":"5px solid #949494", "border-right":"5px solid #949494"})
+    }
+
+});
 function tileImagePicker(mineTileCounter, i){
     if(mineTileCounter===0){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/8/80/Minesweeper_0.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/8/80/Minesweeper_0.svg"></img>`);
     }
     else if(mineTileCounter===1){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Minesweeper_1.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/Minesweeper_1.svg"></img>`);
     }
     else if(mineTileCounter===2){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/4/44/Minesweeper_2.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/4/44/Minesweeper_2.svg"></img>`);
     }
     else if(mineTileCounter===3){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/0/08/Minesweeper_3.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Minesweeper_3.svg"></img>`);
     }
     else if(mineTileCounter===4){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Minesweeper_4.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/4/4f/Minesweeper_4.svg"></img>`);
     }
     else if(mineTileCounter===5){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/4/46/Minesweeper_5.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/4/46/Minesweeper_5.svg"></img>`);
     }
     else if(mineTileCounter===6){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Minesweeper_6.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Minesweeper_6.svg"></img>`);
     }
     else if(mineTileCounter===7){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/5/56/Minesweeper_7.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/5/56/Minesweeper_7.svg"></img>`);
     }
     else if(mineTileCounter===8){
-    $("#"+i).append(`<img class="numberTile" id=${mineTileCounter} src="https://upload.wikimedia.org/wikipedia/commons/0/0d/Minesweeper_8.svg"></img>`);
+    $("#"+i).append(`<img class="numberTile ${mineTileCounter}" id="tile${i}" src="https://upload.wikimedia.org/wikipedia/commons/0/0d/Minesweeper_8.svg"></img>`);
+    $("#"+i).css({"height": "50", "width": "50", "border":"0px"});
     }
-    $("#"+i).css({"height": "50", "width": "50", "border":"0px"})
+    $("#"+i).contents().hide();
 }
 function plantMines(){
     var i = 0;
@@ -114,7 +127,6 @@ function boardPopulate(){
                 mineTileCounter=mineTileCounter+1;
             }
             console.log(mineTileCounter+" mineTileCounte id= "+i);
-            //$("#" + i).append(mineTileCounter);
             tileImagePicker(mineTileCounter, i);
         }
         i=i+1;
